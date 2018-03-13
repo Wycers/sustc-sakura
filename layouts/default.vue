0e599b840364e4fd03a32bac240436c883e90864
Wycer <11711918@mail.sustc.edu.cn>
2018-03-13 11:26:50 +0800

    页面文件

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   v-app
-    v-card(flat)
+    v-card.hidden-sm-and-down(flat)
       v-toolbar(color="primary" flat class="elevation-3")
       v-layout(row pb-2 class="grey lighten-4")
         v-flex(xs8 offset-xs2)
@@ -17,6 +17,17 @@
             v-container
               nuxt
     v-content(class="grey lighten-4")
+      v-card(class="hidden-md-and-up" flat)
+        v-toolbar(card prominent)
+          v-toolbar-title(class="headline") {{ title }}
+          v-spacer
+          v-menu(offset-y)
+            v-btn(slot="activator" icon)
+              v-icon language
+            v-list
+              v-list-tile(v-for="lang in locales" :key="lang" @click="setlocale(lang)")
+                v-list-tile-title {{ lang }}
+        nuxt
       
     v-footer(height="auto" class="grey darken-3 elevation-3")
       v-layout(row wrap justify-center)
@@ -26,9 +37,9 @@
           v-for="(link, i) in links"
           :key="link"
           :to="tos[i]"
-        ) {{ link }}
+        ) {{ $t(link) }}
         </v-btn>
-        v-flex(xs12 py-3 text-xs-center white--text)
+        v-flex(xs12 py-2 text-xs-center white--text)
           span &copy; 2018 {{ $t('footer.copyright') }}
 </template>
 
@@ -47,8 +58,8 @@
     data () {
       return {
         title: 'SUSTech Sakura!',
-        links: ['Home', 'Service', 'About Us', 'Contact Us'],
-        tos: ['/', '/service', '/about', '/contact']
+        links: ['Home', 'Service', 'About'],
+        tos: ['/', '/service', '/about']
       }
     }
   }
@@ -58,16 +69,12 @@
 .card--flex-toolbar {
   margin-top: -32px;
 }
-.headline {
-  font-family: 'Open Sans'
-}
 .stepper-title {
-  font-family: 'Open Sans';
-  font-size: 1.25em!important;
-  font-weight: 100!important;
+  font-size: 1.25rem!important;
+  font-weight: 250!important;
 }
 .content {
-  font-size: 1.35em;
+  font-size: 1.30rem;
   font-weight: 200;
 }
 </style>
