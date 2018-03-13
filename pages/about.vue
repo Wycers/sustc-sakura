21c6297aa390fc6877cea56162170bb8038ecb37
Wycer <11711918@mail.sustc.edu.cn>
2018-03-13 11:25:32 +0800

    删除联系分页 将联系方式移至关于页面

--- pages/about.vue
+++ pages/about.vue
@@ -1,15 +1,53 @@
-<template>
-  <v-layout>
-    <v-flex text-xs-center>
-      <img src="/v.png" alt="Vuetify.js" class="mb-5">
-      <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
-        <footer>
-          <small>
-            <em>&mdash;John Johnson</em>
-          </small>
-        </footer>
-      </blockquote>
-    </v-flex>
-  </v-layout>
+<template lang="pug">
+v-container
+  v-layout(row wrap)
+    v-flex(text-xs-center xs12 sm4 md4 lg4 xl4)
+      v-avatar(
+        :size="size"
+        class="grey lighten-4"
+      )
+        img(src="~static/avatar.jpg" alt="avatar")
+    v-flex(xs12 sm8 md8 lg8 xl8)
+      div.ma-3
+        div.my-3
+          span.title Wycer
+        div.my-3
+          span.contact 11711918@mail.sustc.edu.cn
+        div.my-3
+          span.body-1 全幼儿园最可爱！
 </template>
+<script>
+export default {
+  computed: {
+    size () {
+      switch (this.$vuetify.breakpoint.name) {
+        case 'xs': return 120
+        case 'sm': return 120
+        case 'md': return 150
+        case 'lg': return 180
+        case 'xl': return 180
+      }
+    }
+  }
+}
+</script>
+
+
+<style>
+.title {
+  font-size: 28px!important;
+  font-weight: 200;
+}
+.contact {
+  font-weight: 200;
+  font-size: 20px
+}
+.body-1 {
+  font-size: 22px!important;
+  font-weight: 200!important;
+  font-family: "Roboto";
+}
+.center {
+  text-align: center
+}
+</style>
