7609e5838a7b5bf52fb1aee79c23ecd723e8ff52
Wycer <11711918@mail.sustc.edu.cn>
2018-03-28 21:27:08 +0800

    小修改

--- pages/service.vue
+++ pages/service.vue
@@ -14,12 +14,12 @@ v-container
 
   div.headline 未经测试的部分
   ol.content
-    li 安卓设备兼容性未知。
+    li 安卓设备兼容性未知。(目前已知华为自带浏览器好像会出问题XD)
     li Windows设备兼容性无法保证。（部分Windows设备可能因为设备自身原因，导入的所有时间均提前一小时）
 
   p.content ……
   p.content
-    <strong> 如果你遇到了任何BUG，抱歉以及烦请将BUG的情况发送至<code>11711918@mail.sustc.edu.cn</code>，我将尽力尽快修复BUG</strong>qwq
+    b 如果你遇到了任何BUG，抱歉以及烦请将BUG的情况发送至<code>11711918@mail.sustc.edu.cn</code>，我将尽力尽快修复BUG qwq
   div.headline 本项目的开发离不开以下项目的贡献
   ol.content
     li Nuxt.js — Vue.js 通用应用框架
